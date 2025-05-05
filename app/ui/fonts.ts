1460638940c154906ce20d5128d5b80e4bf15a52
MrEkwunife <ekwunifechimobi16@gmail.com>
2025-05-05 11:56:46 +0100

    some font corrections

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,7 @@
 import { Inter, Lusitana } from "next/font/google";
 
-export const inter = Inter({ subsets: ["Latin"] });
+export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = Lusitana({
-	subsets: ["Latin"],
+	subsets: ["latin"],
 	weight: ["400", "700"],
 });
