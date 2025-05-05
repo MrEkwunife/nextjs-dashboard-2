9be82ff1cf3538f17dbc9cfcac54b8f8a0909575
MrEkwunife <ekwunifechimobi16@gmail.com>
2025-05-05 11:49:36 +0100

    setting up

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["Latin"] });
+export const lusitana = Lusitana({
+	subsets: ["Latin"],
+	weight: ["400", "700"],
+});
